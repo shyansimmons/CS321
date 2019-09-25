005ad06460f573e8ed25df720528a7346e37e62c
shyansimmons <32111989+shyansimmons@users.noreply.github.com>
2019-09-25 17:12:14 -0400

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,2 +1,90 @@
-This is where I put anything I want hello world
-^X^C
+<!DOCTYPE html> 
+<html lang="en">
+  <head>
+    <!--CS:321 Shyan Yen HTML Skeleton 
+        Assignment 2 Due Sept. 25th -->
+    <meta charset="UTF-8">
+    <meta name="description" content="This is my Wellness Website">
+    <title>Shyan's Wellness Website</title>
+    <link src="/css/style.css>" rel="stylesheet" />
+  </head>
+  <body>
+  <header>
+    <!--Website title-->
+    <h1>The Wellness Site</h1>
+    <!--Blurb under website title-->
+    <h2>A guide to a healthy lifestyle</h2>
+    </header>
+    <br> 
+    <!--Navigation Container-->
+    <div class="navbar">
+    <!--Section 1 Container-->
+    <div class="sec1">
+          <!--About section-->
+        <button>ABOUT</button>
+      <ul class="about">
+        <li><a href="#sitecreator">Site creator</a></li>
+        <li><a href="#mission">Mission</a></li>
+        </ul>
+    </div>
+        <!--Section 2 Container-->
+    <div class="sec2">
+          <!--Mindfulness section-->
+        <button>MINDFULNESS</button>
+      <ul class="mindfulness">
+        <li><a href="#meditation">Meditation</a></li>
+        <li><a href="#journaling">Journaling</a></li>
+        </ul>
+    </div>
+    <!--Section 3 Container-->
+    <div class="sec3">
+          <!--Mindfulness section-->
+        <button>DIET</button>
+      <ul class="diet">
+        <li><a href="#overall">Overall Diet</a></li>
+        <li><a href="#weightloss">Weight Loss</a></li>
+        <li><a href="#weightgain">Weight Gain</a></li>
+        </ul>
+    </div>
+     <!--Section 4 Container-->
+    <div class="sec4">
+          <!--About section-->
+        <button>EXERCISE</button>
+      <ul class="exercise">
+        <li><a href="#stretching">Stretching</a></li>
+        <li><a href="#cardio">Cardio</a></li>
+        <li><a href="#strength">Strength</a></li>
+        </ul>
+    </div>
+         <!--Section 5 Container-->
+    <div class="sec5">
+          <!--Spirituality section-->
+        <button>SPIRITUALITY</button>
+      <ul class="spirit">
+        <li><a href="#religious">Religious</a></li>
+        <li><a href="#nonreligious">Non-religious</a></li>
+        </ul>
+    </div>
+         <!--Section 6 Container-->
+    <div class="sec6">
+          <!--Other Tips section-->
+        <button>OTHER TIPS</button>
+      <ul class="other">
+        <li><a href="#lovedones">Helping Loved Ones</a></li>
+        <li><a href="#recsites">Recommended Sites</a></li>
+        </ul>
+    </div>
+    </div>
+    <!--Wellness definition wrapper-->
+    <div class="def-wrapper">
+      <p><strong>wellness</strong> [wel-nis]
+      <br><i>noun</i>
+      <br>
+      <br>1. the quality or state of being healthy in body and mind, especially as the result of deliberate effort.
+      <br>
+      <br><i>Source: dictionary.com</i>
+      </p>
+    </div>
+    </body>
+  </html>
+    
